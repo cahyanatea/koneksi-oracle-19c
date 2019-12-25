e826144c0c14ea78891f49cc99f0228326edfb4a
Asep Cahyana <cahyanatea003@gmail.com>
2019-12-25 09:53:46 +0700

    query untuk import data jika mode CREATE actif

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO siswa (nama, umur, jenis_kelamin, alamat) VALUES ('Naruto', 10, 'LAKI-LAKI', 'Uzumaki');
+INSERT INTO siswa (nama, umur, jenis_kelamin, alamat) VALUES ('Sasuke', 11, 'LAKI-LAKI', 'Uchiha');
+INSERT INTO siswa (nama, umur, jenis_kelamin, alamat) VALUES ('Sakura', 9, 'PEREMPUAN', 'Konoha');

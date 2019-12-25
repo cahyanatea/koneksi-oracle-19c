94ba5a9bbd8af11d41a07f1349114c23c3858a14
Asep Cahyana <cahyanatea003@gmail.com>
2019-12-25 09:55:52 +0700

    Entity untuk data siswa - strategy IDENTITY

---
+++ src/main/java/com/example/demo/entity/Siswa.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author okt19
+ */
+@Entity
+@Table(name = "siswa")
+public class Siswa {
+
+    // gunakan IDENTITY -- primary key di database - unique
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_siswa")
+    private Integer idSiswa;
+
+    private String nama;
+    private Integer umur;
+
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+
+    private String alamat;
+
+    public Integer getIdSiswa() {
+        return idSiswa;
+    }
+
+    public void setIdSiswa(Integer idSiswa) {
+        this.idSiswa = idSiswa;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Integer getUmur() {
+        return umur;
+    }
+
+    public void setUmur(Integer umur) {
+        this.umur = umur;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+}
